b14c02f3345fbd269dc6e0b5555d16f16655aaa2
Ashok-AutomationTest <ashokcan3@gmail.com>
2021-05-16 19:34:17 -0700

    modify

--- GithubDemo/src/com/githubdemo/Githubdemo.java
+++ GithubDemo/src/com/githubdemo/Githubdemo.java
@@ -1,8 +1,15 @@
 package com.githubdemo;
+import java.util.List;
+import java.util.Set;
 
 public class Githubdemo {
+	private void demo() {
+		// TODO Auto-generated method stub
+	System.out.println("welcome");
+	}
 
 public static void main(String[] args) {
 	System.out.println("Hi Welcome to GitHub Repository");
+	Githubdemo g=new Githubdemo();
 }		
 }
